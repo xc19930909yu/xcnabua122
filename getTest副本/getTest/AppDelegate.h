86bf731b55cf9fdbfeaa4a2c90a64d4e87e10fb1
xc19930909yu <qianfeng@qianfengdeMac-mini-6.local>
2016-03-04 16:49:08 +0800

    修改后的
    修改了

---
+++ getTest副本/getTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  getTest
+//
+//  Created by qianfeng on 16/3/4.
+//  Copyright © 2016年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ getTest副本/getTest/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  getTest
+//
+//  Created by qianfeng on 16/3/4.
+//  Copyright © 2016年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@property(nonatomic,copy) NSString *str;
+
+@property(nonatomic,copy)NSString *str123;
+@end
+
